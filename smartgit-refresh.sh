ddce78aab6d92192198c692f7a42f26089941428
Emre Altınok <38796980+emre6nok@users.noreply.github.com>
2021-11-01 16:50:54 +0300

    remove backup folder

--- smartgit-refresh.sh
+++ smartgit-refresh.sh
@@ -2,13 +2,15 @@
 # SmartGit Refresh Trial-Time
 # Creator: Emre Altınok
 
-version="21.1"
-folderName="/Users/$USER/Library/Preferences/SmartGit/${version}/"
-files=("license" "preferences.yml")
+folderName="/Users/$USER/Library/Preferences/SmartGit/21.1/"
+backupName=".backup"
+files=("license" "preferences.yml" "settings.xml")
 
 for file in "${files[@]}" 
 do
    rm -i ${folderName}${file}
 done
+
+rm -rf $folderName$backupName
 
 echo "SmartGit successfully reset trial time."
